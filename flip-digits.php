2503e60c13ceea56a0c1e90c5694f8efad5377a2
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-11 23:12:38 +0200

    added-flip-digits

---
+++ flip-digits.php
@@ -0,0 +1,15 @@
+<?php
+
+function flipDigits($num){
+    $num = (string) $num;
+    $flipped_num ="";
+
+    for($i=strlen($num)-1; $i>=0; $i--){
+        $flipped_num .= $num[$i];
+    }
+    $flipped_num = (int) $flipped_num;
+}
+
+
+$number = 123;
+flipDigits($number);
