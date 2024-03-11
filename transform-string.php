5387435f9adbccebf93a5f6c18876be5dd687ec5
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-11 22:56:13 +0200

    added-transform-string

--- transform-string.php
+++ transform-string.php
@@ -1,3 +1,58 @@
 <?php
-$x = 1;
-echo $x;
+
+$operation = "12+20*30";
+
+function transformStringToOperation($input_str) {
+    $numbers = [];
+    $operators = [ "=", "+", "-", "*", "/", "%"];
+    $current = "";
+
+    for($i=0; $i <= strlen($input_str)-1; $i++){
+        $character = $input_str[$i];
+        if(!in_array($character, $operators)){
+            $current .= $character;
+        }
+        elseif(!empty($current)){
+            $numbers[] = (int)$current;
+            $numbers[] = $character;
+            $current = "";
+        } 
+    }
+    if (!empty($current)) {
+        $numbers[] = (int)$current;
+    }
+
+    $result = $numbers[0];
+    for($j=1; $j<count($numbers); $j+=2){
+        $operator = $numbers[$j];
+        $number = $numbers[$j+1];
+
+        switch($operator){
+            case "+":
+                $result += $number;
+                break;
+
+            case "-":
+                $result -= $number;
+                break;
+
+            case "*":
+                $result *= $number;
+                break;
+
+            case "/":
+                $result /= $number;
+                break;
+            
+            case "%":
+                $result %= $number;
+                break;        
+        }
+
+    }
+    echo $result;
+    
+}
+transformStringToOperation($operation);
+
+// Does not handle BODMAS.
